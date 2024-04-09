ee0f5949b92219929a30a0e95368509f63c8faa4
Andrei Shabaev <shabaevandrew@gmail.com>
2024-04-09 09:35:43 +0300

    First commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,8 +8,8 @@ def configuration():
     browser.config.window_width = 1920
     browser.config.window_height = 1080
     browser.config.base_url = 'https://demoqa.com'
-    # options = webdriver.ChromeOptions()
-    # options.page_load_strategy = 'none'
-    # browser.config.driver_options = options
+    options = webdriver.ChromeOptions()
+    options.page_load_strategy = 'none'
+    browser.config.driver_options = options
     yield
     browser.quit()
