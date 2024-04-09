7c1d2ef3d555f9ef7bf647d84a3605e81fef609b
Andrei Shabaev <shabaevandrew@gmail.com>
2024-04-09 19:34:07 +0300

    fix after comments

--- demoqa_tests/model/pages/registration_page.py
+++ demoqa_tests/model/pages/registration_page.py
@@ -1,22 +1,19 @@
 import os
-
 from selene import browser, have, command
+
+import tests
 
 
 class RegistrationPage:
     def __init__(self):
         self.state = browser.element('#state')
 
-
     def open(self):
         browser.open('/automation-practice-form')
-        # browser.all('[id^=google_ads][id$=container__]').with_(timeout=10).wait_until(
-        #     have.size_greater_than_or_equal(3)
-        # )
-        # browser.all('[id^=google_ads][id$=container__]').perform(command.js.remove)
 
     def should_open_page_with_title(self, name):
-        browser.should(have.title('DEMOQA'))
+        browser.should(have.title(name))
+
     def fill_first_name(self, value):
         browser.element('#firstName').type(value)
 
@@ -49,12 +46,14 @@ class RegistrationPage:
             browser.all('.custom-checkbox').element_by(have.exact_text(hob)).click()
 
     def fill_photo(self, file):
-        browser.element('#uploadPicture').send_keys(os.path.abspath(f'resources/{file}'))
-
+        browser.element('#uploadPicture').set_value(
+            os.path.abspath(
+                os.path.join(os.path.dirname(tests.__file__), f'resources/{file}')
+            )
+        )
 
     def fill_address(self, value):
         browser.element('#currentAddress').type(value)
-
 
     def fill_state(self, value):
         self.state.perform(command.js.scroll_into_view)
@@ -62,7 +61,6 @@ class RegistrationPage:
         browser.all('[id^=react-select][id*=option]').element_by(
             have.exact_text(value)
         ).click()
-
 
     def fill_city(self, value):
         browser.element('#city').click()
@@ -76,7 +74,8 @@ class RegistrationPage:
     def should_open_form_with_text(self, value):
         browser.element('.modal-title').should(have.text(value))
 
-    def should_registered_user_with(self, full_name , email, gender, phone_number, birthday, subjects, hobbies, photo, address, city ):
+    def should_registered_user_with(self, full_name, email, gender, phone_number, birthday, subjects, hobbies, photo,
+                                    address, city):
         browser.element('.table').all('td').even.should(have.exact_texts(
             full_name,
             email,
--- tests/test_students_form.py
+++ tests/test_students_form.py
@@ -1,44 +1,27 @@
-import os
-from selene import browser, be, have, command
-
 from demoqa_tests.model.pages.registration_page import RegistrationPage
 
 
 def test_fill_and_send():
     registration_page = RegistrationPage()
-
     registration_page.open()
 
     registration_page.should_open_page_with_title('DEMOQA')
 
     registration_page.fill_first_name('Akhil')
-
     registration_page.fill_last_name('Nadar')
-
     registration_page.fill_email('nadar@test.in')
-
     registration_page.choose_gender('Male')
-
     registration_page.fill_phone_number('9112345678')
-
     registration_page.fill_birthday('1992', 'July', '31')
-
     registration_page.fill_subjects('Maths', 'Physics')
-
     registration_page.choose_hobbies('Sports', 'Reading')
-
     registration_page.fill_photo('avatar.jpg')
-
     registration_page.fill_address('14, Ashoka Rd, Sansad Marg Area')
-
     registration_page.fill_state('NCR')
-
     registration_page.fill_city('Delhi')
-
     registration_page.submit()
 
     registration_page.should_open_form_with_text('Thanks for submitting the form')
-
     registration_page.should_registered_user_with(
         'Akhil Nadar',
         'nadar@test.in',
@@ -51,4 +34,3 @@ def test_fill_and_send():
         '14, Ashoka Rd, Sansad Marg Area',
         'NCR Delhi'
     )
-
