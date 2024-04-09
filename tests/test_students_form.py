2fe92c85d114618c56209be982efa026ccb559ae
Andrei Shabaev <shabaevandrew@gmail.com>
2024-04-09 09:44:06 +0300

    New branch

--- tests/test_students_form.py
+++ tests/test_students_form.py
@@ -1,30 +1,45 @@
 import os
 from selene import browser, be, have, command
 
+from demoqa_tests.model.pages.registration_page import RegistrationPage
+
+
 def test_fill_and_send():
-    browser.open('/automation-practice-form')
-    browser.should(have.title('DEMOQA'))
-    browser.element('#firstName').type('Akhil')
-    browser.element('#lastName').type('Nadar')
-    browser.element('#userEmail').type('nadar@test.in')
-    browser.element('[for="gender-radio-1"]').click()
-    browser.element('#userNumber').type('9112345678')
-    # browser.element('#dateOfBirthInput').perform(command.select_all).type('31 Jul 1992').press_enter()
-    browser.element('#dateOfBirthInput').click()
-    browser.element('.react-datepicker__year-select').click().element('option[value="1992"]').click()
-    browser.element('.react-datepicker__month-select').click().element('option[value="6"]').click()
-    browser.element('.react-datepicker__day--031').click()
-    browser.element('#subjectsInput').type('Maths').press_enter().type('Physics').press_enter()
-    browser.element('[for="hobbies-checkbox-1"]').click()
-    browser.element('[for="hobbies-checkbox-2"]').click()
-    browser.element('#uploadPicture').send_keys(os.path.abspath('avatar.jpg'))
-    browser.element('#currentAddress').type('14, Ashoka Rd, Sansad Marg Area')
-    browser.element('#react-select-3-input').type('NCR').press_enter()
-    browser.element('#react-select-4-input').type('Delhi').press_enter()
-    browser.element('#submit').perform(command.js.scroll_into_view).click()
-    
-    browser.element('.modal-title').should(have.text('Thanks for submitting the form'))
-    browser.element('.table').all('td').even.should(have.exact_texts(
+    registration_page = RegistrationPage()
+
+    registration_page.open()
+
+    registration_page.should_open_page_with_title('DEMOQA')
+
+    registration_page.fill_first_name('Akhil')
+
+    registration_page.fill_last_name('Nadar')
+
+    registration_page.fill_email('nadar@test.in')
+
+    registration_page.choose_gender('Male')
+
+    registration_page.fill_phone_number('9112345678')
+
+    registration_page.fill_birthday('1992', 'July', '31')
+
+    registration_page.fill_subjects('Maths', 'Physics')
+
+    registration_page.choose_hobbies('Sports', 'Reading')
+
+    registration_page.fill_photo('avatar.jpg')
+
+    registration_page.fill_address('14, Ashoka Rd, Sansad Marg Area')
+
+    registration_page.fill_state('NCR')
+
+    registration_page.fill_city('Delhi')
+
+    registration_page.submit()
+
+    registration_page.should_open_form_with_text('Thanks for submitting the form')
+
+    registration_page.should_registered_user_with(
         'Akhil Nadar',
         'nadar@test.in',
         'Male',
@@ -35,7 +50,5 @@ def test_fill_and_send():
         'avatar.jpg',
         '14, Ashoka Rd, Sansad Marg Area',
         'NCR Delhi'
-
-    )
     )
 
